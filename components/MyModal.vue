5d21e7438fd75a282d79ae3fa84e3fc5ce5dca90
Dima <dima5447298@gmail.com>
2018-06-29 19:03:45 +0300

    modal

--- components/MyModal.vue
+++ components/MyModal.vue
@@ -1,25 +1,23 @@
 <template lang="pug">
 
-.Modal_Overlay(v-show="show", @click.self='close')
-  .Modal_Card(:class='[mod, position]')
-    .close(v-if='position', @click='close')
-
+.Modal-rootOverlay(v-show="show", @click.self='close')
+  .Modal-Card(:class='[mod, position]')
     .title(v-if="title") {{ title }}
     .body
       //slot(name="body")
       //Заполните <div slot="body">Контент</div>
       slot
       
-      .actions
-        .btn.-red(@click='close') Ок
-        //.btn.-lite.-circle Cancel
-        //span Cancel
+    .actions
+      slot(name="actions")
+        .btn.red(@click='close') close
+
 </template>
 
 <script>
 
 export default {
-  //name: 'modal',
+  name: 'modal',
   props: {
     show: Boolean,
     mod: String,
@@ -29,32 +27,17 @@ export default {
       default: 'Y_center'
     }
   },
-  // watch: {
-  //   show(isShow) {
-  //     document.documentElement.style.cssText = (isShow) ? "height:100vh;overflow:hidden;" : "";
-  //   }
-  // },
+  watch: {
+    show(isShow) {
+      document.body.style.cssText = (isShow) ? "height:100vh;overflow:hidden;" : "";
+    }
+  },
   methods: {
     close() {
-      this.$emit('update:show', false)
+      this.$emit('update:show', false);
+      document.body.style = '';
     },
-    // Hide() {
-    //   this.vis = false
-    // }
   }
-  // methods: {
-  //   close() {
-  //     this.$emit('close');
-  //     document
-  //       .body
-  //       .classList
-  //       .remove('no-scroll');
-  //   }
-  // },
-  // destroyed () {
-  //   document
-  //     .body.classList.remove('no-scroll')
-  // }
 }
 
 </script>
@@ -64,67 +47,71 @@ export default {
 
 @import '../assets/settings'
 
+$bg = #f1f1f1;
 
-.Modal_Overlay
-  z-index 100
-  position fixed
-  left 0
-  top 0
-  width 100% //100vw
-  height 100%
-  display flex
-  cursor pointer
-  background rgba(#444 0.7)
-  
-  overflow hidden
-  overflow-y overlay //auto
+.Modal
+  &-rootOverlay
+    z-index 100
+    position fixed
+    left 0
+    top 0
+    width 100% //100vw
+    height 100vh
+    display flex
+    cursor pointer
+    background rgba(#444 0.7)
+    //background rgba(#444 0.7) url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 20 20'><g stroke='white' stroke-width='4' stroke-linecap='round'><line x1='3' y1='3' x2='17' y2='17'/><line x1='3' y1='17' x2='17' y2='3'/></g></svg>") top right no-repeat
+    
+    overflow hidden
+    overflow-y overlay //auto
 
-.Modal_Card
-  //reset
-  cursor auto
-  background #FFF
-  
-  &.toCenter
-    margin auto
+  &-Card
+    //reset rootOverlay
+    cursor auto
+    background #FFF
+
+.Modal-Card
+  .title
+    font-size 1.4rem
+    line-height 1.3
+    padding: .75rem 1.5rem;
+    //border-radius .2em 0 0
+  .body
+    background #f1f1f1
+    padding 1rem 1.5rem;
+    //fix
+    p
+      margin-bottom 0
+  .actions
+    text-align right
+    padding: .75rem 1.5rem 1.2rem
+
   &.XL
     max-width 1080px
     margin auto
   &.SM
     max-width 460px
 
-.Modal_Card
-  .title
-    font-size 1.4rem
-    line-height 1.3
-    
-    background #FFF
-    border-radius 5px 0 0
-  .title
-  .body
-    padding: .75rem 1.5rem;
-     
-     
-//position    
+  &.toCenter
+    margin auto
+//position
 .Y_center
-  margin 18% auto auto
+  margin 10% auto auto
+  
+  border-radius .2em
   width 90%
   max-width 780px
-   
-  //border-top .5em solid $blue //#03a9f4
-  border-radius 5px
-  background #f1f1f1
   
-  animation Y_center .2s .3s cubic-bezier(0.250, 0.460, 0.450, 0.940) forwards	//ease-in
   transform scale(.5)
   opacity 0
-  .title
-    border-radius 5px 0 0
+  animation Y_center .2s .3s cubic-bezier(0.250, 0.460, 0.450, 0.940) forwards	//ease-in
 @keyframes Y_center
   to
     transform scale(1)
-    opacity 1  
+    opacity 1
+
+.lModal
 .rModal
-  margin-left auto
   //overflow overlay
   padding 1em
   max-width 400px
@@ -132,33 +119,17 @@ export default {
   align-self start
   min-height 100%
   
-  animation rModal .2s .3s cubic-bezier(0.250, 0.460, 0.450, 0.940) forwards	//ease-in
+  animation translateX .2s .3s cubic-bezier(0.250, 0.460, 0.450, 0.940) forwards	//ease-in
+  
+.rModal
+  margin-left auto
   transform translateX(100%)
-  .body
-    background #f1f1f1 
-@keyframes rModal
+.lModal
+  margin-right auto
+  transform: translateX(-100%)
+@keyframes translateX
   to
     transform translateX(0)  
-    
-    
-
-//Close
-$finger = 44px 
-.Y_center
-  position relative //for close
-  .close
-    cursor pointer
-    position absolute
-    top -($finger/2)
-    right -($finger/2)
-    width $finger
-    height $finger
-    border-radius 50%
-    background $blue url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 20 20'><g stroke='white' stroke-width='4' stroke-linecap='round'><line x1='3' y1='3' x2='17' y2='17'/><line x1='3' y1='17' x2='17' y2='3'/></g></svg>") center no-repeat
-    &:hover
-      background-color $blue + 20
-
-
 
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -69,7 +69,7 @@
     legend form-group
     .flex-row
       .form-group
-        input.Serch(type='text')
+        input.Search(type='text')
         select(multiple)
           each option in ['red','green','orange','lite','circle']
             option #{option}
@@ -204,7 +204,7 @@
   hr.H1
   h1 Модалки <b>!!!TODO</b>
   .form-line
-    each item,i in ['Modal', 'Modal(toCenter)', 'Small Modal', 'Small(toCenter)', 'Large Modal', 'Right Modal']
+    each item,i in ['Modal', 'Modal(toCenter)', 'Small Modal', 'Small(toCenter)', 'Large Modal', 'Right Modal', 'Left Modal']
       button.btn.-blue(@click="showModal_"+(i+1)+" = true") #{item}
 
   my-modal(:show.sync="showModal_1", title="Base Modal")
@@ -230,6 +230,10 @@
     p Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod dignissimos voluptate iure eius cupiditate rem sed consequuntur itaque odio. Assumenda dignissimos veritatis placeat nesciunt maiores vel fuga est odio magnam!
     h4 Magnam atque, accusamus deleniti, inventore pariatur voluptates eos officiis quasi officia magni reprehenderit ipsa nam ducimus, temporibus autem sunt odio quae iste.
 
+  my-modal(:show.sync="showModal_7", position="lModal", title="Lorem ipsum dolor")
+    h2 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    p Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod dignissimos voluptate iure eius cupiditate rem sed consequuntur itaque odio. Assumenda dignissimos veritatis placeat nesciunt maiores vel fuga est odio magnam!
+    h4 Magnam atque, accusamus deleniti, inventore pariatur voluptates eos officiis quasi officia magni reprehenderit ipsa nam ducimus, temporibus autem sunt odio quae iste.
 
   hr.H1
 
@@ -279,7 +283,8 @@ export default {
       showModal_3: false,
       showModal_4: false,
       showModal_5: false,
-      showModal_6: false
+      showModal_6: false,
+      showModal_7: false
     };
   }
 };
